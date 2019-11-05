8a0781e2ad8cd671c563e11e97648c47ee5e7110
shibatamasayuki <masayuki.shibata.0831@gmail.com>
2019-11-05 16:23:54 +0900

    first commit

---
+++ homework_a.py
@@ -0,0 +1,9 @@
+users = ["Bob", "Tom", "Ken"]
+
+int_numbers = [1, 2, 3, 4, 5]
+
+kazuma_info = ["Kazuma", "Takahashi", 35]
+
+members = ["Kazuma", "Makoto", "Ohira"]
+print(members[1])
+print(members[0])
